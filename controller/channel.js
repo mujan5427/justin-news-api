83df2df3e65480dd7a2b77dd095e6d293016eb9d
Justin Ho <mujan5427@gmail.com>
2017-04-19 17:52:38 +0800

    fix issue

--- controller/channel.js
+++ controller/channel.js
@@ -2,22 +2,51 @@ var db = require('../modules/database.js');
 
 module.exports = function(req, res) {
 
-  // console.log(req);
+  var
+  channel_type,
+  page_limit,
+  page_offset,
+  current_page;
 
-  var sql = `
-  SELECT news_id, main_image, title, description, author, publish_date
-  FROM news
-  WHERE channel_type = '國際'
-  LIMIT 10`;
+  var channel_type_mapping = {
+    'international': '國際',
+    'finance': '財經',
+    'politics': '政治',
+    'literature-and-art': '藝文',
+  };
+
+  if (req.query !== {}) {
+
+    channel_type = req.query.channel_type;
+    channel_type = channel_type_mapping[channel_type];
+
+    page_limit = Number(req.query.news_count_per_page);
+    current_page = Number(req.query.current_page);
+    page_offset = (current_page * page_limit) - page_limit;
+  }
+
+  var sql = `SELECT count(*) AS page_total FROM news`;
 
   db.query(sql, function (err, rows, fields) {
 
-    var data_count = rows.length;
-    var response_data = {'news_amount': 'yes'};
+    var page_total = Math.ceil(rows[0].page_total / page_limit);
 
-    response_data.data = rows;
+    var sql = `
+    SELECT news_id, main_image, title, description, author, publish_date
+    FROM news
+    WHERE channel_type = '${channel_type}'
+    ORDER BY publish_date DESC
+    LIMIT ${page_offset}, ${page_limit}`;
 
-    res.send(response_data);
+    db.query(sql, function (err, rows, fields) {
+
+      var response_data = {'page_total': page_total};
+
+      response_data.data = rows;
+
+      res.send(response_data);
+
+    });
   });
 
 
